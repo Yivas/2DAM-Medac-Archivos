9d05f2762fa7785c9c4874612782055f7e31bcfb
pamo007 <59517021+Yivas@users.noreply.github.com>
2022-11-08 14:17:05 +0100

    Final 08/11/2022

---
+++ Accesoadatos/StringTool/AccesoDatosTema5/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS EJERCICIO1;
+CREATE TABLE EJERCICIO1(ID INT PRIMARY KEY,NOMBRE VARCHAR(50),APELLIDOS VARCHAR(50),DIRECCION VARCHAR(50),LOCALIDAD VARCHAR(50),CP INT,TELEFONO INT,MAIL VARCHAR(50),FECHA_NACIMIENTO DATE);
+
+INSERT INTO EJERCICIO1 VALUES(1,'Juan','Garcia Lopez','Calle 2','Madrid',28100,123456789,'correo@gmail.com','1990-01-01');
+INSERT INTO EJERCICIO1 VALUES(2,'Pedro','Lopez Lopez','Avenida 1','Sevilla',28045,823256789,'pd@hotmail.com','1995-03-02');
+INSERT INTO EJERCICIO1 VALUES(3,'Marcos','Martinez Echevarria','Calle 3','Madrid',28070,163453182,'marcos@gmail.com','1992-05-03');
+INSERT INTO EJERCICIO1 VALUES(4,'Maria','Gonzalez Perez','Calle 4','Barcelona',29002,123456789,'barce@hotmail.com','2003-12-21');
+INSERT INTO EJERCICIO1 VALUES(5,'Ana','Perez Perez','Calle 5','Zaragoza',34002,118276789,'anaperez@gmail.com','1998-06-12');
+INSERT INTO EJERCICIO1 VALUES(6,'Luis','Garcia Lopez','Avenida 6','Valencia',50902,728451289,'galoluis@gmail.com','1997-07-11');
+INSERT INTO EJERCICIO1 VALUES(7,'Antonio','Rivas Lopez','Calle 6','Madrid',28002,115556789,'anrivaslo@hotmail.com','1981-08-10');
+INSERT INTO EJERCICIO1 VALUES(8,'Jose','Garcia Lopez','Calle 1','Badalona',98101,153651729,'bad98@outlook.com','1999-09-09');
+INSERT INTO EJERCICIO1 VALUES(9,'Luis','Martinez Gonzalez','Avenida 9','Madrid',28022,915356789,'mangon@gmail.com','1996-10-08');
+INSERT INTO EJERCICIO1 VALUES(10,'Maria','Garcia Lopez','Calle 10','Madrid',28002,123456789,'mariagarcia@gmail.com','1990-11-07');
