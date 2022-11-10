be37aeee404873c1ea11bf099ab76a70be461681
pamo007 <59517021+Yivas@users.noreply.github.com>
2022-11-10 14:37:31 +0100

    Final dia 10/11/2022

---
+++ Accesoadatos/StringTool/AccesoDatosTema5/src/main/java/com/example/demo/Actividad6.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Actividad6 {
+    
+    
+    // private static String jdbcUrl = "jdbc:h2:mem:test";
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mariadb://localhost:3306/tema6";
+    static String usuario = "root";
+    static String password = "root";
+
+
+    public static void main(String[] args) throws SQLException {
+        
+        // Hacer una selección de los alumnos entre 24 y 28 años ordenado dicha selección por 
+        // edad de la base de datos de MariaDB
+        
+        Connection connection;
+        connection = DriverManager.getConnection(URL, usuario, password);
+        System.out.println("Conexión realizada correctamente");
+
+        String sql = "SELECT * FROM ALUMNOS WHERE YEAR(FECHA_NACIMIENTO) BETWEEN 1993 AND 1997 ORDER BY FECHA_NACIMIENTO";
+
+        
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        
+
+        while (resultSet.next()) {
+            System.out.println("Nombre: " + resultSet.getString("NOMBRE")
+                + " Apellidos: " + resultSet.getString("APELLIDOS")
+                + " Fecha de nacimiento: " + resultSet.getString("FECHA_NACIMIENTO"));
+        }
+
+
+        
+
+    }
+
+}
